0a62577e0fa3faf5f0138a62a68294a3ac4d278d
sri <sri@sris-MBP.attlocal.net>
2025-05-10 11:46:16 -0500

    Update

--- cypress.config.js
+++ cypress.config.js
@@ -5,14 +5,16 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   reporter: 'cypress-mochawesome-reporter',
   reporterOptions: {
-    reportDir: 'cypress/reports',
-    overwrite: false,
-    html: false,
-    json: true
+    charts: true,
+    reportPageTitle: 'custom-title',
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
   },
   e2e: {
     setupNodeEvents(on, config) {
       require('cypress-mochawesome-reporter/plugin')(on);
+      return config;
     },
   },
 });
