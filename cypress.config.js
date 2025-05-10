c3b48f48d5cb225fd8c764d5469fe8907df39ef0
sri <sri@sris-MBP.attlocal.net>
2025-05-10 11:37:38 -0500

    Add files

--- cypress.config.js
+++ cypress.config.js
@@ -2,34 +2,17 @@
 
 const { defineConfig } = require("cypress");
 
-
 module.exports = defineConfig({
-
   reporter: 'cypress-mochawesome-reporter',
-
-  video: false,
-
   reporterOptions: {
-
-    charts: true,
-
-    reportPageTitle: 'Cypress Inline Reporter',
-
-    embeddedScreenshots: true, 
-
-    inlineAssets: true, //Adds the asserts inline
-
+    reportDir: 'cypress/reports',
+    overwrite: false,
+    html: false,
+    json: true
   },
-
-
   e2e: {
-
     setupNodeEvents(on, config) {
-
       require('cypress-mochawesome-reporter/plugin')(on);
-
     },
-
   },
-
 });
