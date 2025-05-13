dcc2f7a119a6c9ca7e428e28d87e567bd6e21086
sri <sri@sris-MBP.attlocal.net>
2025-05-13 08:39:02 -0500

    Update config

--- cypress.config.js
+++ cypress.config.js
@@ -3,19 +3,33 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  "reporter": "cypress-multi-reporters",
-  "reporterOptions": {
-    "reporterEnabled": "mochawesome",
-    "mochawesomeReporterOptions": {
-      "reportDir": "cypress/reports",
-      "overwrite": false,
-      "html": false,
-      "json": true
-    }
+  // "reporter": "cypress-multi-reporters",
+  // "reporterOptions": {
+  //   "reporterEnabled": "mochawesome",
+  //   "mochawesomeReporterOptions": {
+  //     "reportDir": "cypress/reports",
+  //     "overwrite": false,
+  //     "html": false,
+  //     "json": true
+  //   }
+  // },
+
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    charts: true,
+    reportPageTitle: 'custom-title',
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+    reportDir: "cypress/reports",
+    overwrite: false,
+    html: false,
+    json: true
   },
+
   e2e: {
     setupNodeEvents(on, config) {
-      // require('cypress-mochawesome-reporter/plugin')(on);
+      require('cypress-mochawesome-reporter/plugin')(on);
       return config;
     },
   },
